549c97fae652161f2e691375eef34e58f059524e
박성현 <pjhomg5530@naver.com>
2023-05-30 15:15:53 +0900

    :heavy_plus_sign: :: Hilt, Retrofit, OkHttp dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id(ProjectProperties.Gradle.APPLICATION)
     id(ProjectProperties.Gradle.KOTLIN)
+    id(ProjectProperties.Gradle.HILT_PLUGIN)
+    kotlin(ProjectProperties.Gradle.KAPT)
 }
 
 android {
@@ -42,4 +44,7 @@ dependencies {
     testImplementation(Dependency.Test.JUNIT)
     androidTestImplementation(Dependency.Test.ANDROID_JUNIT)
     androidTestImplementation(Dependency.Test.ESPRESSO)
+
+    implementation(Dependency.Google.HILT)
+    kapt(Dependency.Google.HILT_COMPILER)
 }
