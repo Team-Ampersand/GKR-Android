55ea097e95cafcb91a90e41d5ed1cd67c3e3f122
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-06-30 16:05:08 +0900

    :lipstick: :: 기자재 filter item card 생성

---
+++ presentation/src/main/java/com/mpersand/presentation/view/main/component/GKRFilterItem.kt
@@ -0,0 +1,38 @@
+package com.mpersand.presentation.view.main.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import com.mpersand.presentation.R
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun GKRFilterItem(
+    title: String,
+    onClick: () -> Unit
+) {
+    Card(
+        shape = RoundedCornerShape(7.dp),
+        border = BorderStroke(width = 1.dp, color = Color(0xFFC9C9C9)),
+        onClick = onClick
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 10.dp, top = 5.dp, end = 10.dp, bottom = 5.dp),
+            text = title,
+            style = TextStyle(
+                fontFamily = FontFamily(Font(R.font.inter_light)),
+                color = Color(0xFFC9C9C9)
+            )
+        )
+    }
+}
