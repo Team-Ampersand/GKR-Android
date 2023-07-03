fd11838ca9244744944c9d9555ee0bc4d8bb7067
박성현 <pjhomg5530@naver.com>
2023-07-03 17:55:47 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/mpersand/data/dto/order/request/OrderRequest.kt
+++ data/src/main/java/com/mpersand/data/dto/order/request/OrderRequest.kt
@@ -1,8 +1,17 @@
 package com.mpersand.data.dto.order.request
 
+import com.mpersand.domain.model.order.request.OrderRequestModel
+
 data class OrderRequest(
     val equipmentId: String,
     val reason: String,
     val state: String,
     val decision: String
+)
+
+fun OrderRequestModel.asOrderRequest() = OrderRequest(
+    equipmentId = equipmentId,
+    reason = reason,
+    state = state,
+    decision = decision
 )
