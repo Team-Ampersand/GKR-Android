9a865c3ac420997a17f0edbcbfd6a24bedcd800b
박성현 <pjhomg5530@naver.com>
2023-07-03 16:06:04 +0900

    :pencil2: :: 함수명 바꾸기

--- data/src/main/java/com/mpersand/data/repository/EquipmentRepositoryImpl.kt
+++ data/src/main/java/com/mpersand/data/repository/EquipmentRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.mpersand.data.repository
 
-import com.mpersand.data.dto.order.response.asEquipmentResponseImpl
+import com.mpersand.data.dto.order.response.asEquipmentResponseModel
 import com.mpersand.data.remote.datasource.equipment.EquipmentDataSource
 import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
 import com.mpersand.domain.repository.EquipmentRepository
@@ -9,5 +9,5 @@ import javax.inject.Inject
 class EquipmentRepositoryImpl @Inject constructor(
     private val equipmentDataSource: EquipmentDataSource
 ): EquipmentRepository {
-    override suspend fun getAllEquipments(): List<EquipmentResponseModel> = equipmentDataSource.getAllEquipments().map { it.asEquipmentResponseImpl() }
+    override suspend fun getAllEquipments(): List<EquipmentResponseModel> = equipmentDataSource.getAllEquipments().map { it.asEquipmentResponseModel() }
 }
