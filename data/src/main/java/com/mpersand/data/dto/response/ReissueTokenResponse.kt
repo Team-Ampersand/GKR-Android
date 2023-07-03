23bfce3b19d6b23ffc94b7d201a87c87da96b927
박성현 <pjhomg5530@naver.com>
2023-07-03 15:38:29 +0900

    :fire: :: 안 쓰이는 model 지우기

--- data/src/main/java/com/mpersand/data/dto/response/ReissueTokenResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.mpersand.data.dto.response
-
-data class ReissueTokenResponse(
-    val accessToken: String,
-    val refreshToken: String,
-    val accessTokenExp: String,
-    val refreshTokenExp: String
-)
