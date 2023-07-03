154f5683175bdc42dc4df9dc90a5a3c5d6ddf795
박성현 <pjhomg5530@naver.com>
2023-07-03 20:09:59 +0900

    :sparkles: :: UiState 적용

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/AuthViewModel.kt
@@ -1,11 +1,14 @@
 package com.mpersand.presentation.viewmodel
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mpersand.domain.model.auth.request.SignInRequestModel
 import com.mpersand.domain.usecase.auth.SaveTokenUseCase
 import com.mpersand.domain.usecase.auth.SignInUseCase
+import com.mpersand.presentation.util.UiState
+import com.mpersand.presentation.util.exceptionHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,22 +18,27 @@ class AuthViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase,
     private val saveTokenUseCase: SaveTokenUseCase
 ) : ViewModel() {
+    private val _signInUiState = MutableLiveData<UiState<Nothing>>()
+    val signInUiState: LiveData<UiState<Nothing>> = _signInUiState
     fun signIn(code: String, navigateToMain: () -> Unit) {
         viewModelScope.launch {
             signInUseCase(SignInRequestModel(code = code))
                 .onSuccess {
-                    Log.d("signIn", it.toString())
                     saveTokenUseCase(
                         accessToken = it.accessToken,
                         refreshToken = it.refreshToken,
                         accessTokenExp = it.accessTokenExp,
                         refreshTokenExp = it.refreshTokenExp
                     )
+                    _signInUiState.value = UiState.Success()
                     navigateToMain()
                 }.onFailure {
-                    Log.d("signIn", it.message.toString())
+                    it.exceptionHandling(
+                        badRequestAction = {
+                            _signInUiState.value = UiState.BadRequest
+                        }
+                    )
                 }
-
         }
     }
 }
