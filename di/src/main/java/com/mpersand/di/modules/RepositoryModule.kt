7a91f87d3109a029d79d57903dc747ef3085cf11
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-03 19:19:17 +0900

    :sparkles: :: user logic module에 추가

--- di/src/main/java/com/mpersand/di/modules/RepositoryModule.kt
+++ di/src/main/java/com/mpersand/di/modules/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.mpersand.di.modules
 
 import com.mpersand.data.repository.AuthRepositoryImpl
 import com.mpersand.data.repository.EquipmentRepositoryImpl
+import com.mpersand.data.repository.UserRepositoryImpl
 import com.mpersand.domain.repository.AuthRepository
 import com.mpersand.domain.repository.EquipmentRepository
+import com.mpersand.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,4 +23,9 @@ interface RepositoryModule {
     fun bindsEquipmentRepository(
         equipmentRepositoryImpl: EquipmentRepositoryImpl
     ): EquipmentRepository
+
+    @Binds
+    fun bindsUserRepository(
+        userRepositoryImpl: UserRepositoryImpl
+    ): UserRepository
 }
