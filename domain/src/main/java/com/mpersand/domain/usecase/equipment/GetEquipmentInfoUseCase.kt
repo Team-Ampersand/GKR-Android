96d7f24a5141c7c606a404255e4fa0b2e2af9d9c
박성현 <pjhomg5530@naver.com>
2023-07-03 18:21:46 +0900

    :sparkles: :: kotlin.runCatching 추가

--- domain/src/main/java/com/mpersand/domain/usecase/equipment/GetEquipmentInfoUseCase.kt
+++ domain/src/main/java/com/mpersand/domain/usecase/equipment/GetEquipmentInfoUseCase.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 class GetEquipmentInfoUseCase @Inject constructor(
     private val equipmentRepository: EquipmentRepository
 ) {
-    suspend operator fun invoke(productNumber: String) = equipmentRepository.getEquipmentInfo(productNumber)
+    suspend operator fun invoke(productNumber: String) = kotlin.runCatching { equipmentRepository.getEquipmentInfo(productNumber) }
 }
