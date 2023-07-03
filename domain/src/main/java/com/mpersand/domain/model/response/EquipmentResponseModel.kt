02d4d1b240722a2c922f50a4e456de26d253a875
박성현 <pjhomg5530@naver.com>
2023-07-03 15:26:54 +0900

    :sparkles: :: model 생성

---
+++ domain/src/main/java/com/mpersand/domain/model/response/EquipmentResponseModel.kt
@@ -0,0 +1,9 @@
+package com.mpersand.domain.model.response
+
+data class EquipmentResponseModel(
+    val productNumber: String,
+    val name: String,
+    val image: String,
+    val description: String,
+    val rentStatus: String
+)
