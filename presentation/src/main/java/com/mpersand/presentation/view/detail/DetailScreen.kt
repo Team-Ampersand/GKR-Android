69920a11c979eded346fe5d01ee5073aef744bd7
박성현 <pjhomg5530@naver.com>
2023-07-05 14:27:14 +0900

    :sparkles: :: 스낵바 만들기

--- presentation/src/main/java/com/mpersand/presentation/view/detail/DetailScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/detail/DetailScreen.kt
@@ -1,7 +1,5 @@
 package com.mpersand.presentation.view.detail
 
-import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -13,22 +11,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,6 +35,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.mpersand.presentation.R
 import com.mpersand.presentation.util.UiState
 import com.mpersand.presentation.viewmodel.DetailViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun DetailScreen(
@@ -43,84 +43,102 @@ fun DetailScreen(
     productNumber: String?,
     viewModel: DetailViewModel = hiltViewModel()
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+    val detailUiState by viewModel.getEquipmentInfoUiState.observeAsState()
+    val coroutineScope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+
     LaunchedEffect(Unit) {
         viewModel.getEquipmentInfo(checkNotNull(productNumber))
     }
 
-    var showDialog by remember { mutableStateOf(false) }
-    val detailUiState by viewModel.getEquipmentInfoUiState.observeAsState()
-
-    if (showDialog) {
-        DetailDialog {
-            showDialog = false
-        }
-    }
-
-    when (val state = detailUiState) {
-        UiState.Loading -> {}
-        is UiState.Success -> {
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
-                    .background(Color.White)
-            ) {
-                Image(
-                    modifier = modifier.fillMaxWidth(),
-                    painter = rememberAsyncImagePainter(state.data!!.image),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop
-                )
-                Column(modifier = modifier.padding(horizontal = 26.dp)) {
-                    Text(
-                        modifier = modifier.padding(top = 15.dp),
-                        text = state.data!!.name,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Thin,
-                        fontFamily = FontFamily(Font(R.font.fraunces_black)),
-                        color = Color(0xFF000000)
-                    )
-                    Text(
-                        modifier = modifier.padding(top = 3.dp),
-                        text = when (state.data!!.name) {
-                            "맥북" -> { "#맥북  #노트북" }
-                            "갤럭시 북" -> { "#갤럭시 북  #노트북" }
-                            "터치모니터" -> { "#터치모니터  #모니터" }
-                            else -> { "#?" }
-                        },
-                        fontSize = 8.sp,
-                        fontWeight = FontWeight.Thin,
-                        fontFamily = FontFamily(Font(R.font.fraunces_black)),
-                        color = Color(0xFF999999)
-                    )
-                    Text(
-                        modifier = modifier.padding(top = 30.dp),
-                        text = state.data!!.description,
-                        fontSize = 13.sp,
-                        fontWeight = FontWeight.Thin,
-                        fontFamily = FontFamily(Font(R.font.fraunces_black)),
-                        color = Color(0xFF000000)
-                    )
-                    Spacer(modifier = modifier.weight(1f))
-                    Button(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .height(55.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color(0xFFFFA559),
-                            contentColor = Color.White
-                        ),
-                        shape = RoundedCornerShape(10.dp),
-                        onClick = { showDialog = true }
-                    ) {
-                        Text(text = "대여하기")
-                    }
-                    Spacer(modifier = modifier.height(50.dp))
+    Scaffold(scaffoldState = scaffoldState) { paddingValues ->
+        if (showDialog) {
+            DetailDialog(onDismissRequest = { showDialog = false }) {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("대여 기간은 1달입니다.")
                 }
             }
         }
-        UiState.BadRequest -> {}
-        UiState.Unauthorized -> {}
-        UiState.NotFound -> {}
-        else -> {}
+
+        when (val state = detailUiState) {
+            UiState.Loading -> {}
+            is UiState.Success -> {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                        .padding(paddingValues)
+                ) {
+                    Image(
+                        modifier = modifier.fillMaxWidth(),
+                        painter = rememberAsyncImagePainter(state.data!!.image),
+                        contentDescription = "image",
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(modifier = modifier.padding(horizontal = 26.dp)) {
+                        Text(
+                            modifier = modifier.padding(top = 15.dp),
+                            text = state.data!!.name,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Thin,
+                            fontFamily = FontFamily(Font(R.font.fraunces_black)),
+                            color = Color(0xFF000000)
+                        )
+                        Text(
+                            modifier = modifier.padding(top = 3.dp),
+                            text = when (state.data!!.name) {
+                                "맥북" -> {
+                                    "#맥북  #노트북"
+                                }
+
+                                "갤럭시 북" -> {
+                                    "#갤럭시 북  #노트북"
+                                }
+
+                                "터치모니터" -> {
+                                    "#터치모니터  #모니터"
+                                }
+
+                                else -> {
+                                    "#?"
+                                }
+                            },
+                            fontSize = 8.sp,
+                            fontWeight = FontWeight.Thin,
+                            fontFamily = FontFamily(Font(R.font.fraunces_black)),
+                            color = Color(0xFF999999)
+                        )
+                        Text(
+                            modifier = modifier.padding(top = 30.dp),
+                            text = state.data!!.description,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Thin,
+                            fontFamily = FontFamily(Font(R.font.fraunces_black)),
+                            color = Color(0xFF000000)
+                        )
+                        Spacer(modifier = modifier.weight(1f))
+                        Button(
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .height(55.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFFFA559),
+                                contentColor = Color.White
+                            ),
+                            shape = RoundedCornerShape(10.dp),
+                            onClick = { showDialog = true }
+                        ) {
+                            Text(text = "대여하기")
+                        }
+                        Spacer(modifier = modifier.height(50.dp))
+                    }
+                }
+            }
+            UiState.BadRequest -> {}
+            UiState.Unauthorized -> {}
+            UiState.NotFound -> {}
+            else -> {}
+        }
     }
 }
