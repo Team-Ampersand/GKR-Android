31548c96654133d258781a1bd8a54e986dae5dac
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-05 10:46:05 +0900

    :sparkles: :: order repository 자신이 대여한 기자재 리스트 추가

--- domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
+++ domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
@@ -1,7 +1,10 @@
 package com.mpersand.domain.repository
 
+import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
 import com.mpersand.domain.model.order.request.OrderRequestModel
 
 interface OrderRepository {
+    suspend fun getEquipmentRentalByUserList(): List<EquipmentResponseModel>
+
     suspend fun postRentalRequest(orderRequest: OrderRequestModel)
 }
