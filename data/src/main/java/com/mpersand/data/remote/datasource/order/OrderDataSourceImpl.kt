90bb726d536d195c35cd519f6995da7754664642
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-05 10:45:48 +0900

    :sparkles: :: order dataSource에 자신이 대여한 기자재 리스트 추가

--- data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSourceImpl.kt
+++ data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.mpersand.data.remote.datasource.order
 
 import com.mpersand.data.dto.order.request.OrderRequest
+import com.mpersand.data.dto.order.response.EquipmentResponse
 import com.mpersand.data.network.api.OrderApi
 import com.mpersand.data.util.safeApiCall
 import javax.inject.Inject
@@ -8,5 +9,7 @@ import javax.inject.Inject
 class OrderDataSourceImpl @Inject constructor(
     private val orderApi: OrderApi
 ): OrderDataSource {
+    override suspend fun getEquipmentRentalByUserList(): List<EquipmentResponse> = safeApiCall { orderApi.getEquipmentRentalByUserList() }
+
     override suspend fun postRentalRequest(orderRequest: OrderRequest) = safeApiCall { orderApi.postRentalRequest(orderRequest) }
 }
