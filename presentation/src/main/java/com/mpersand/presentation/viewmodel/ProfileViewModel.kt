ce195c8aa4cfabce70d265c41dea4c4b1f6b1037
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-05 11:08:45 +0900

    :sparkles: :: viewModel 자신이 대여한 기자재 리스트 로직 추가

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/ProfileViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/ProfileViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
 import com.mpersand.domain.model.response.UserResponseModel
+import com.mpersand.domain.usecase.order.EquipmentRentalByUserUseCase
 import com.mpersand.domain.usecase.user.GetUserUseCase
 import com.mpersand.domain.usecase.user.LogoutUseCase
 import com.mpersand.presentation.util.UiState
@@ -16,13 +18,17 @@ import javax.inject.Inject
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val getUserUseCase: GetUserUseCase,
-    private val logoutUseCase: LogoutUseCase
+    private val logoutUseCase: LogoutUseCase,
+    private val equipmentRentalByUserUseCase: EquipmentRentalByUserUseCase
 ): ViewModel() {
     private val _getUser: MutableLiveData<UiState<UserResponseModel?>> = MutableLiveData()
     val getUser: LiveData<UiState<UserResponseModel?>> = _getUser
 
     private val _logout: MutableLiveData<UiState<Unit>> = MutableLiveData()
     val logout: LiveData<UiState<Unit>> = _logout
+
+    private val _getListByUser: MutableLiveData<UiState<List<EquipmentResponseModel>>> = MutableLiveData()
+    val getListByUser: LiveData<UiState<List<EquipmentResponseModel>>> = _getListByUser
 
     fun getUser() {
         viewModelScope.launch {
@@ -58,6 +64,26 @@ class ProfileViewModel @Inject constructor(
                     },
                     notFoundAction = {
                         _logout.value = UiState.NotFound
+                    }
+                )
+            }
+        }
+    }
+
+    fun getEquipmentRentalListByUser() {
+        viewModelScope.launch {
+            equipmentRentalByUserUseCase().onSuccess {
+                _getListByUser.value = UiState.Success(it)
+            }.onFailure {
+                it.exceptionHandling(
+                    badRequestAction = {
+                        UiState.BadRequest
+                    },
+                    unauthorizedAction = {
+                        UiState.Unauthorized
+                    },
+                    notFoundAction = {
+                        UiState.NotFound
                     }
                 )
             }
