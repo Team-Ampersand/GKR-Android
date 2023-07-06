8468ee42a19d0c76df0ba30404fa0c1711253296
박성현 <pjhomg5530@naver.com>
2023-07-06 19:37:04 +0900

    :sparkles: :: 프로필 정보 가져오기 로직 생성

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/MainViewModel.kt
@@ -1,12 +1,15 @@
 package com.mpersand.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
+import com.mpersand.domain.model.response.UserResponseModel
 import com.mpersand.domain.usecase.equipment.EquipmentFilterUseCase
 import com.mpersand.domain.usecase.equipment.GetAllEquipmentsUseCase
+import com.mpersand.domain.usecase.user.GetUserUseCase
 import com.mpersand.presentation.util.UiState
 import com.mpersand.presentation.util.exceptionHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,10 +19,15 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getAllEquipmentsUseCase: GetAllEquipmentsUseCase,
+    private val getUserUseCase: GetUserUseCase,
     private val equipmentFilterUseCase: EquipmentFilterUseCase
 ): ViewModel() {
     private val _getAllEquipmentsUiState = MutableLiveData<UiState<List<EquipmentResponseModel>>>()
-    val getAllEquipmentsUiState: LiveData<UiState<List<EquipmentResponseModel>>> = _getAllEquipmentsUiState
+    val getAllEquipmentsUiState: LiveData<UiState<List<EquipmentResponseModel>>> =
+        _getAllEquipmentsUiState
+
+    private val _getUserInfoUiState = MutableLiveData<UiState<UserResponseModel?>>()
+    val getUserInfoUiState: LiveData<UiState<UserResponseModel?>> = _getUserInfoUiState
 
     private val _mainFilter = MutableLiveData<UiState<List<EquipmentResponseModel>>>()
     val mainFilter: LiveData<UiState<List<EquipmentResponseModel>>> = _mainFilter
@@ -28,9 +36,11 @@ class MainViewModel @Inject constructor(
         viewModelScope.launch {
             getAllEquipmentsUseCase()
                 .onSuccess {
+                    Log.d("TAG", "getAllEquipments: $it")
                     _getAllEquipmentsUiState.value = UiState.Success(it)
                 }
                 .onFailure {
+                    Log.d("TAGe", "getAllEquipments: $it")
                     it.exceptionHandling(
                         badRequestAction = {
                             _getAllEquipmentsUiState.value = UiState.BadRequest
@@ -40,6 +50,30 @@ class MainViewModel @Inject constructor(
                         },
                         notFoundAction = {
                             _getAllEquipmentsUiState.value = UiState.NotFound
+                        }
+                    )
+                }
+        }
+    }
+
+    fun getUserInfo() {
+        viewModelScope.launch {
+            getUserUseCase()
+                .onSuccess {
+                    Log.d("TAG", "getUserInfo: $it")
+                    _getUserInfoUiState.value = UiState.Success(it)
+                }
+                .onFailure {
+                    Log.d("TAGe", "getUserInfo: $it")
+                    it.exceptionHandling(
+                        badRequestAction = {
+                            _getUserInfoUiState.value = UiState.BadRequest
+                        },
+                        unauthorizedAction = {
+                            _getUserInfoUiState.value = UiState.Unauthorized
+                        },
+                        notFoundAction = {
+                            _getUserInfoUiState.value = UiState.NotFound
                         }
                     )
                 }
