d43482a4ce8af26b7a49152f8c8dc587d6928ecf
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-06 13:26:43 +0900

    :sparkles: :: main 필터 로직 생성

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/MainViewModel.kt
@@ -1,11 +1,11 @@
 package com.mpersand.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
+import com.mpersand.domain.usecase.equipment.EquipmentFilterUseCase
 import com.mpersand.domain.usecase.equipment.GetAllEquipmentsUseCase
 import com.mpersand.presentation.util.UiState
 import com.mpersand.presentation.util.exceptionHandling
@@ -15,10 +15,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getAllEquipmentsUseCase: GetAllEquipmentsUseCase
+    private val getAllEquipmentsUseCase: GetAllEquipmentsUseCase,
+    private val equipmentFilterUseCase: EquipmentFilterUseCase
 ): ViewModel() {
     private val _getAllEquipmentsUiState = MutableLiveData<UiState<List<EquipmentResponseModel>>>()
     val getAllEquipmentsUiState: LiveData<UiState<List<EquipmentResponseModel>>> = _getAllEquipmentsUiState
+
+    private val _mainFilter = MutableLiveData<UiState<List<EquipmentResponseModel>>>()
+    val mainFilter: LiveData<UiState<List<EquipmentResponseModel>>> = _mainFilter
+
     fun getAllEquipments() {
         viewModelScope.launch {
             getAllEquipmentsUseCase()
@@ -38,6 +43,26 @@ class MainViewModel @Inject constructor(
                         }
                     )
                 }
+        }
+    }
+
+    fun mainFilter(keyword: String) {
+        viewModelScope.launch {
+            equipmentFilterUseCase(keyword).onSuccess {
+                _mainFilter.value = UiState.Success(it)
+            }.onFailure {
+                it.exceptionHandling(
+                    badRequestAction = {
+                        _mainFilter.value = UiState.BadRequest
+                    },
+                    unauthorizedAction = {
+                        _mainFilter.value = UiState.Unauthorized
+                    },
+                    notFoundAction = {
+                        _mainFilter.value = UiState.NotFound
+                    }
+                )
+            }
         }
     }
 }
